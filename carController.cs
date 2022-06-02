471fa8210ee3fad1eaad29ce2f82edd12afb8afa
joanaviolante <106742866+joanaviolante@users.noreply.github.com>
2022-06-02 16:00:04 +0100

    Add files via upload

---
+++ carController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour {
+
+	public float carSpeed;
+	Vector3 position;
+	public UiManager ui;
+	public audioManagerScript am;
+
+	public trackMove trackMov;
+	void Start () {
+		InvokeRepeating ("StarSound",9f,12000f);
+		position = transform.position;
+
+	}
+
+	void StarSound(){
+		am.carSound.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		position.x  = position.x +Input.GetAxis ("Horizontal")*carSpeed *Time.deltaTime;
+		Debug.Log (transform.position);
+		Debug.Log ("Time.deltaTime "+Time.deltaTime);
+
+		position.x=Mathf.Clamp (position.x, -2.1f, 2.1f);
+			transform.position=position;
+		
+
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Enemy Car") {
+			Destroy (gameObject);
+			ui.gameOver ();
+			am.carSound.Stop ();
+			trackMov.speed = 0;
+		}
+	}
+}
